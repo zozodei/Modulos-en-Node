fba7519371ff719b3eec957a31df7473481d1d66
Zoe Acquistapace <zodoze8@gmail.com>
2026-03-20 11:36:49 -0300

    ejercicio 2

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-//ejercicio 1
+//Ejercicio 1
 
 import fs from 'fs';
 
-const contenido = fs.readFileSync('productos.json', 'utf-8');
+/*  const contenido = fs.readFileSync('productos.json', 'utf-8');
 const productos = JSON.parse(contenido);
 
 productos.forEach(e => {
     console.log(`Producto: ${e.nombre}
         Precio: ${e.precio}
         Id: ${e.id}`);
-});
+});  */
+
+//Ejercicio 2
+
+/*const agregarProducto = (nombre, precio) => 
+    {
+
+        const contenido = fs.readFileSync('productos.json', 'utf-8');
+        const productos = JSON.parse(contenido);
+
+        const nuevoId = productos[productos.length -1].id +1 ;
+
+        const nuevoProducto = 
+        {
+            id: nuevoId,
+            nombre: nombre,
+            precio: precio
+        }
+        
+         productos.push (nuevoProducto);
+
+        fs.writeFileSync (`productos.json`, JSON.stringify (productos, null, 2));
+
+   
+
+    }
+
+    agregarProducto("Hamburguesa", 30000); */
+    
+    
